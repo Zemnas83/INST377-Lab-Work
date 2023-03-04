5422eea4e573cc49724919b4f610a819e931319e
zmat83 <zayanmatin@gmail.com>
2023-03-04 17:24:43 -0500

    lab 5 initial changes

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,11 +3,29 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
+
+}
+
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
+
+
+
+  let currentList = [];
+  
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
+
+   
 
     /*
       ## GET requests and Javascript
@@ -36,6 +54,9 @@ async function mainEvent() { // the async keyword means we can make API requests
     */
 
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+
+
+    currentList = await results.json();
     /*
    ## Get request with query parameters
 
@@ -56,7 +77,24 @@ async function mainEvent() { // the async keyword means we can make API requests
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
   });
+
+
+  filterButton.addEventListener('click', (event) => {
+    console.log("clicked filterButton");
+
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+  });
+
+
+
 }
+
+
 
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
