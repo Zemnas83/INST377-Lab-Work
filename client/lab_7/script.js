30cd8e6fd0b2ae7c3a5960046f78796ed0e1ad38
zmat83 <zayanmatin@gmail.com>
2023-04-02 14:05:09 -0400

    lab 7 changes

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,15 +1,3 @@
-/* eslint-disable max-len */
-
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
-
-/*
-  ## Utility Functions
-    Under this comment place any utility functions you need - like an inclusive random number selector
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
 
 
 
@@ -33,20 +21,7 @@ function processRestaurants(list) {
       const str = `<li>${item.name}</li>`;
       target.innerHTML += str;
     });
-    /*
-    ## JS and HTML Injection
-      There are a bunch of methods to inject text or HTML into a document using JS
-      Mainly, they're considered "unsafe" because they can spoof a page pretty easily
-      But they're useful for starting to understand how websites work
-      the usual ones are element.innerText and element.innerHTML
-      Here's an article on the differences if you want to know more:
-      https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
-  
-    ## What to do in this function
-      - Accept a list of restaurant objects
-      - using a .forEach method, inject a list element into your index.html for every element in the list
-      - Display the name of that restaurant and what category of food it is
-  */
+ 
   }
   
   function filterList(list, query) {
@@ -71,90 +46,102 @@ function processRestaurants(list) {
   
   async function mainEvent() {
     
-    const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+    const mainForm = document.querySelector('.main_mainForm'); // get your main mainForm so you can do JS with it
     const submit = document.querySelector('button[type="submit"]'); // get a reference to your submit button
     const filterDataButton = document.querySelector('.filter');
     const loadDataButton = document.querySelector('#data_load');
     const generateListButton = document.querySelector('#generate');
-    const loadAnimation = document.querySelector('#data_load_animation')
-  
+    const textField = document.querySelector('#resto');
+   
+   
+    const loadAnimation = document.querySelector('#data_load_animation');
     loadAnimation.style.display = 'none';
-    submit.style.display = 'none'; // let your submit button disappear
+    generateListButton.style.display = 'none';
   
   
-  
+    let storedList = [];
+    let currentList = [];
     
-    const results = await fetch('/api/foodServicesPG');
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
   
     
     console.table(arrayFromJson.data);
   
-    
-  
-  
     console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
   
-    // This IF statement ensures we can't do anything if we don't have information yet
+    // This IF statement ensures we can't do anything if we don't have inmainFormation yet
     if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
       submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
   
-      // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-      // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-      form.addEventListener('submit', (submitEvent) => {
+      
+      mainForm.addEventListener('submit', (submitEvent) => {
         // This is needed to stop our page from changing to a new URL even though it heard a GET request
         submitEvent.preventDefault();
   
         // This constant will have the value of your 15-restaurant collection when it processes
         const restaurantList = processRestaurants(arrayFromJson.data);
   
-        // And this function call will perform the "side effect" of injecting the HTML list for you
+        // And this function call will permainForm the "side effect" of injecting the HTML list for you
         injectHTML(restaurantList);
   
-        // By separating the functions, we open the possibility of regenerating the list
-        // without having to retrieve fresh data every time
-        // We also have access to some form values, so we could filter the list based on name
       });
     }
+
+    loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+    
+      console.log('loading data'); // this is substituting for a "breakpoint"
+    
+     
+      loadAnimation.style.display = 'inline-block';
+    
+      const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    
+    
+      storedList = await results.json();
+
+      if (storedList.length > 0) {
+        generateListButton.classList.remove('hidden');
+      }
+    
+      loadAnimation.style.display = 'none';
+      console.table(storedList);
+    
+    });
+    
+    filterDataButton.addEventListener('click', (event) => {
+      console.log("clicked filterButton");
+    
+      const mainFormData = new mainFormData(mainmainForm);
+      const mainFormProps = Object.fromEntries(mainFormData);
+    
+      console.log(mainFormProps);
+      const newList = filterList(currentList, mainFormProps.resto);
+    
+      console.log(newList); // this is called "dot notation"
+      
+      injectHTML(newList);
+    
+    })
+    
+    generateListButton.addEventListener('click', (event) => {
+      console.log('generate new list');
+      currentList = cutRestaurantList(storedList);
+      console.log(currentList);
+      injectHTML(currentList);
+    })
+
+    textField.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      const newList = filterList(currentList, event.target.value);
+      console.log(newList);
+      injectHTML(newList);
+
+    })
+
   }
   
-  loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-    
-    console.log('loading data'); // this is substituting for a "breakpoint"
-  
-   
-    loadAnimation.style.display = 'inline-block';
-  
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-  
-  
-    currentList = await results.json();
-  
-    loadAnimation.style.display = 'none';
-    console.table(currentList);
-  
-  });
-  
-  filterButton.addEventListener('click', (event) => {
-    console.log("clicked filterButton");
-  
-    const formData = new FormData(mainForm);
-    const formProps = Object.fromEntries(formData);
-  
-    console.log(formProps);
-    const newList = filterList(currentList, formProps.resto);
-  
-    console.log(newList); // this is called "dot notation"
-    
-    injectHTML(newList);
-  
-  });
-  
-  generateListButton.addEventListener('click', (event) => {
-    console.log('generate new list');
-    const restaurantsList = cutRestaurantList(currentList);
-    injectHTML(restaurantsList);
-  })
+ 
   
   document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
   
